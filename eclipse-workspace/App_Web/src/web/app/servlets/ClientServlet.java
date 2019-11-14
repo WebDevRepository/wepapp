d06987eaa851df134a3632d293f691b2f80793d8
anas <anas@PC>
2019-11-14 17:19:39 +0100

    cmt

---
+++ eclipse-workspace/App_Web/src/web/app/servlets/ClientServlet.java
@@ -0,0 +1,68 @@
+package web.app.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import web.app.model.Client;
+
+/**
+ * Servlet implementation class ClientServlet
+ */
+public class ClientServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String nom = request.getParameter( "nomClient" );
+        String prenom = request.getParameter( "prenomClient" );
+        String adresse = request.getParameter( "adresseClient" );
+        String telephone = request.getParameter( "telephoneClient" );
+        String email = request.getParameter( "emailClient" );
+
+        String message;
+        /*
+         * Initialisation du message à afficher : si un des champs obligatoires
+         * du formulaire n'est pas renseigné, alors on affiche un message
+         * d'erreur, sinon on affiche un message de succès
+         */
+        if ( nom.trim().isEmpty() || adresse.trim().isEmpty() || telephone.trim().isEmpty() ) {
+            message = "Erreur - Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\"creerClient.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'un client.";
+        } else {
+            message = "Client créé avec succès !";
+        }
+        /*
+         * Création du bean Client et initialisation avec les données récupérées
+         */
+        Client client = new Client();
+        client.setNom( nom );
+        client.setPrenom( prenom );
+        client.setAdresse( adresse );
+        client.setTelephone( telephone );
+        client.setEmail( email );
+
+        /* Ajout du bean et du message à l'objet requête */
+        request.setAttribute( "client", client );
+        request.setAttribute( "message", message );
+
+        /* Transmission à la page JSP en charge de l'affichage des données */
+        this.getServletContext().getRequestDispatcher( "/afficherClient.jsp" ).forward( request, response );
+    }
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
